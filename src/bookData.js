284ec2622930f0ef56f5b388ae24731ff2305469
steviekaye <stevie.kaye@gmail.com>
2019-02-21 17:26:45 +1300

    Init + WIP bookdata

---
+++ src/bookData.js
@@ -0,0 +1,74 @@
+const bookData = {
+  books: [
+    {
+      id: 1,
+      title: 'Happy Dreams',
+      year: 2007,
+      cover: '/images/happy_dreams.jpg',
+      titleCN: '高兴',
+      titlePinyin: 'Gaoxing',
+      synopsis: "todo",
+      price: 35
+    },
+    {
+      id: 2,
+      title: 'Ruined City',
+      year: 1993,
+      cover: '/images/ruined_city.jpg',
+      titleCN: '废都',
+      titlePinyin: 'Fei Du',
+      synopsis: "todo",
+      price: 35
+    },
+    {
+      id: 3,
+      title: 'The Earthen Gate',
+      year: 1996,
+      cover: '/images/the_earthen_gate.jpg',
+      titleCN: '土门',
+      titlePinyin: 'Tu Men',
+      synopsis: "todo",
+      price: 40
+    },
+    {
+      id: 4
+      title: 'The Lantern Bearer',
+      year: 2013,
+      cover: '/images/the_lantern_bearer.jpg',
+      titleCN: '带灯',
+      titlePinyin: 'Dai Deng',
+      synopsis: "todo",
+      price: 30
+    },
+    {
+      id: 5
+      title: 'Shaanxi Opera',
+      year: 2005,
+      cover: '/images/shaanxi_opera.jpg',
+      titleCN: '秦腔',
+      titlePinyin: 'Qin Qiang',
+      synopsis: "todo",
+      price: 50
+    },
+    {
+      id: 6
+      title: 'Turbulence',
+      year: 1987,
+      cover: '/images/turbulence.jpg',
+      titleCN: '浮躁',
+      titlePinyin: 'Fuzao',
+      synopsis: "todo",
+      price: 25
+    },
+    {
+      id: 7
+      title: 'Old Kiln Village',
+      year: 2011,
+      cover: '/images/old_kiln_village.jpg',
+      titleCN: '古炉',
+      titlePinyin: 'Gu Lu',
+      synopsis: "todo",
+      price: 45
+    },
+  ]
+}
