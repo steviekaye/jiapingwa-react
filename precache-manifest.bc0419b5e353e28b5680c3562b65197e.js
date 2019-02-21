8b1e5316b43b7a8c797bb30520113fe17ec70e7c
steviekaye <stevie.kaye@gmail.com>
2019-02-21 17:43:18 +1300

    Updates

---
+++ precache-manifest.bc0419b5e353e28b5680c3562b65197e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jiapingwa-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "05c7a2e6d5951a9f10bf",
+    "url": "/jiapingwa-react/static/js/runtime~main.05c7a2e6.js"
+  },
+  {
+    "revision": "9007e2a2d0dae8985b97",
+    "url": "/jiapingwa-react/static/js/main.9007e2a2.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/jiapingwa-react/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "9007e2a2d0dae8985b97",
+    "url": "/jiapingwa-react/static/css/main.11cc23b5.chunk.css"
+  },
+  {
+    "revision": "f128eca78b880a560741f65975ab51d9",
+    "url": "/jiapingwa-react/index.html"
+  }
+];
