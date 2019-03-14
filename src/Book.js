eeea264eb84731557f48fdcd89ba23c2c15865f1
steviekaye <stevie.kaye@gmail.com>
2019-03-14 21:30:36 +1300

    Minor tweaks

--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { store } from './redux/store'
 
-function Book({bookData, state, dispatch}) {
+function Book({bookData, dispatch}) {
+  // function Book({bookData, state, dispatch}) {
+  // console.log('store', store);
+  // console.log('store.getState()', store.getState());
+  // console.log('state', state);
+  console.log('dispatch', dispatch);
+  console.log('store', store.getState())
+
   return (
     <div className="book">
       <div>
-        <img src={process.env.PUBLIC_URL + bookData.cover}></img>
+        <img src={process.env.PUBLIC_URL + bookData.cover} alt={bookData.title}></img>
       </div>
       <div className="book-info">
         <div className="book-title">{bookData.title} </div>
@@ -14,6 +22,7 @@ function Book({bookData, state, dispatch}) {
           <span className="book-title-CN">{bookData.titleCN} </span>
           <span className="book-title-pinyin">({bookData.titlePinyin}) </span>
         </div>
+        {}
         <button onClick={() => dispatch({type: 'ADD_TO_CART', payload: bookData.id })}>Add to cart</button>
         <div className="book-price">Price: ${bookData.price}</div>
       </div>
@@ -21,12 +30,10 @@ function Book({bookData, state, dispatch}) {
   )
 }
 
-// const mapStateToProps = (state) => state;
+function mapStateToProps(state) {
+  console.log('state', state);
+  console.log('state2', store.getState());
+  return state;
+  }
 
-// function mapDispatchToProps(dispatch) {
-//   return {
-//     addToCart: () => dispatch({type: 'ADD_TO_CART', payload: bookData.id })
-//   }
-// }
-
-export default connect(state => state)(Book);
+export default connect(mapStateToProps())(Book);
